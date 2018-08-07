7597f0cae7729cdc5f0a57950114e01e371bd280
zhukovka <helen.zhukova@gmail.com>
2018-08-07 09:29:25 +0300

    factoring polynomials

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import {fillContents} from './js/contents';
+import * as math from './js/components/math-expression';
 //build contents
 fillContents();
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,6 @@ module.exports = {
     },
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname)
+        path: path.resolve(__dirname, 'assets/js/')
     }
 };
